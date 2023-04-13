051f1fb6be846f7f32bf92f0bb0607251ecbeebd
Bakuretsy <joaoarmeniosilveira@gmail.com>
2023-04-12 23:37:18 -0300

    sexo

--- index.js
+++ index.js
@@ -15,7 +15,16 @@ const background = new Sprite({
         y: 0
     },
     imageSrc: './assets/background.png'
-})
+});
+
+const shop = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './assets/shop.png',
+    scale: 2.75
+});
 
 const player = new Fighter({
     color: 'blue',
@@ -70,48 +79,15 @@ const keys = {
     }
 }
 
-function rectangularColision({ rectangle1, rectangle2 }) {
-    return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x
-        && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width
-        && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
-        && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-    );
-}
-
-function determineWinner({ player, enemy, timerId }) {
-    clearTimeout(timerId);
-    document.querySelector('#result').style.display = 'flex';
-    if (player.health == enemy.health){
-        document.querySelector('#result').innerHTML = 'Tie';
-    } else if (player.health > enemy.health) {
-        document.querySelector('#result').innerHTML = 'Player Won';
-    } else if (player.health < enemy.health) {
-        document.querySelector('#result').innerHTML = 'Enemy Won';
-    }
-}
-
-let timer = 51;
-let timerId;
-function decreaseTimer() {
-    if (timer > 0){
-        timerId = setTimeout(decreaseTimer, 1000);
-        timer--;
-        document.querySelector('#timer').innerHTML = timer;
-    }
-
-    if (timer === 0) {
-        determineWinner({player, enemy, timerId});
-    }
-}
-
 decreaseTimer();
 
 function animate() {
     window.requestAnimationFrame(animate);
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     background.update();
+    shop.update();
 
     player.update();
     enemy.update();
